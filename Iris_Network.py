26e08ff36fe31cddd981128ced8892612febb733
Edoardo <eduardovaira@gmail.com>
2017-05-30 22:31:35 +0200

    Add files via upload

---
+++ Iris_Network.py
@@ -0,0 +1,121 @@
+# Dependencies
+import csv
+import tensorflow as tf
+from tensorflow.python.framework import ops
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Make results reproducible
+seed = 321
+tf.set_random_seed(seed)
+np.random.seed(seed)
+
+# Read the .csv file
+file = open('Iris_Dataset.csv', newline='')
+reader = csv.reader(file)
+
+# Skip the header ( Id,SepalLengthCm,SepalWidthCm... )
+header = next(reader)
+
+X = []
+y = []
+
+# Load the data in X and y :
+# Id,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Species
+    # Iris-setosa
+    # Iris-versicolor
+    # Iris-virginica
+
+for row in reader:
+    Id = int(row[0])
+    SepalLengthCm = float(row[1])
+    SepalWidthCm = float(row[2])
+    PetalLengthCm = float(row[3])
+    PetalWidthCm = float(row[4])
+    X.append([SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm])
+
+    if row[5] == 'Iris-setosa':
+        y.append(1)
+    elif row[5] == 'Iris-versicolor':
+        y.append(2)
+    elif row[5] == 'Iris-virginica':
+        y.append(3)
+
+X = np.array(X)
+y = np.array(y)
+
+# Create graph session
+sess = tf.Session()
+
+# Split data into Train / Test = 80 % / 20 %
+train_indices = np.random.choice(len(X), round(len(X)*0.8), replace=False)
+test_indices = np.array(list(set(range(len(X))) - set(train_indices)))
+x_vals_train = X[train_indices]
+x_vals_test = X[test_indices]
+y_vals_train = y[train_indices]
+y_vals_test = y[test_indices]
+
+# Normalize by column (min-max norm)
+def normalize_cols(m):
+    col_max = m.max(axis=0)
+    col_min = m.min(axis=0)
+    return (m-col_min) / (col_max - col_min)
+
+x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
+x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
+
+# Declare batch size
+batch_size = 50
+
+# Initialize placeholders
+x_data = tf.placeholder(shape=[None, 4], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+
+# Create variables for both NN layers
+hidden_layer_nodes = 15
+A1 = tf.Variable(tf.random_normal(shape=[4,hidden_layer_nodes])) # inputs -> hidden nodes
+b1 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))   # one biases for each hidden node
+A2 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,1])) # hidden inputs -> 1 output
+b2 = tf.Variable(tf.random_normal(shape=[1]))   # 1 bias for the output
+
+
+# Declare model operations
+hidden_output = tf.nn.relu(tf.add(tf.matmul(x_data, A1), b1))
+final_output = tf.nn.relu(tf.add(tf.matmul(hidden_output, A2), b2))
+
+# Declare loss function (MSE)
+loss = tf.reduce_mean(tf.square(y_target - final_output))
+
+# Declare optimizer
+my_opt = tf.train.GradientDescentOptimizer(0.005)
+train_step = my_opt.minimize(loss)
+
+# Initialize variables
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# Training loop
+loss_vec = []
+test_loss = []
+for i in range(500):
+    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(np.sqrt(temp_loss))
+
+    test_temp_loss = sess.run(loss, feed_dict={x_data: x_vals_test, y_target: np.transpose([y_vals_test])})
+    test_loss.append(np.sqrt(test_temp_loss))
+    if (i+1)%50==0:
+        print('Generation: ' + str(i+1) + '. Loss = ' + str(temp_loss))
+
+# Plot loss (MSE) over time
+plt.plot(loss_vec, 'k-', label='Train Loss')
+plt.plot(test_loss, 'r--', label='Test Loss')
+plt.title('Loss (MSE) per Generation')
+plt.legend(loc='upper right')
+plt.xlabel('Generation')
+plt.ylabel('Loss')
+plt.show()
